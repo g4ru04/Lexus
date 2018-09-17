b30e3ea682ab6611f1e3f61e9ec6a85035f8e323
Ben <g4ru04@gmail.com>
2018-09-17 17:04:34 +0800

    Three page entrances

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/cs_customer', function(req, res, next) {
+  res.render('cs_customer');
+});
+
+router.get('/cs_manager', function(req, res, next) {
+  res.render('cs_manager');
+});
+
+router.get('/cs_manager_list', function(req, res, next) {
+  res.render('cs_manager_list');
 });
 
 module.exports = router;
