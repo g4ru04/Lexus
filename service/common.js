29cab490561e15bf9941496a69b78992f62cebf2
Ben <g4ru04@gmail.com>
2018-09-19 11:56:31 +0800

    Modify data accessing method
    EJ5 prototype declare -> ES6  new class declare

--- service/common.js
+++ service/common.js
@@ -1,12 +1,21 @@
 (function() {
-	function common() {}
-	common.prototype.getSl = function(req, res) {
-		var data = require('./specialist.json', 'utf-8');
-		return res.send(data);
+	
+	class Common{
+		constructor() {
+			this.getSl = this.getSl.bind(this);
+			this.getCl = this.getCl.bind(this);
+		}
+		getSl(req, res) {
+			var data = require('./specialist.json', 'utf-8');
+			return res.send(data);
+		}
+		getCl(req, res) {
+			var data = require('./customer.json', 'utf-8');
+			return res.send(data);
+		}
 	}
-	common.prototype.getCl = function(req, res) {
-		var data = require('./customer.json', 'utf-8');
-		return res.send(data);
-	}
-	module.exports = new common();
+	
+	module.exports = new Common();
+	
 }());
+
