675a187a7186e3d143f2418078b4eec936a87fb4
Ben <g4ru04@gmail.com>
2018-10-18 17:04:59 +0800

    簡易登入介面

--- routes/index.js
+++ routes/index.js
@@ -24,15 +24,17 @@ router.get('/cs_manager_list', function(req, res, next) {
   res.render('cs_manager_list');
 });
 router.get('/serv.do', function(req, res, next) {
+  if(req.query.s==null){
+	  res.render('cs_manager_login');
+  }else{
+    let func_list =["金牌話術","訊息推播","照片","相機","撥打","出價","車主資料","常用訊息"];
   
-  let func_list =["金牌話術","訊息推播","照片","相機","撥打","出價","車主資料","常用訊息"];
-  
-  let list_data = require('../service/specialist.json', 'utf-8');
-  res.render('cs_manager',{
-    list_data: list_data,
-	func_list: func_list
-  });
-  
+    let list_data = require('../service/specialist.json', 'utf-8');
+    res.render('cs_manager',{
+      list_data: list_data,
+	  func_list: func_list
+    });
+  }
 });
 router.get('/serv.do', function(req, res, next) {
   
